180e71ccb8bd5cec61146c91a85a64d67555194a
bcruzguerra <98077654+bcruzguerra@users.noreply.github.com>
2022-02-07 14:38:29 -0300

    Add files via upload
    v2

--- java.java
+++ java.java
@@ -1,10 +1,18 @@
+//========================COMMENTS=============================
+// This is for line comments
+/** This is
+* for
+* Block Comments
+*/
+
+
 package java.basicsyntax;
 
+public class Main {
+	public static void main(String[] args) {
 
-public static void main(String[] args) {
-
-//------------------------------------------------------
-// Variables:
+//=============================================================
+//------------------------VARIABLES----------------------------
 // <visibility> <modifiers> tipe name <=value>;
 // <visibilitye> = public, protected, private
 // <modifiers> = static, final
@@ -25,21 +33,53 @@ final String MGMT = "Kids";
 private String fastisio = "Fastisio.";
 boolean boolBoolBool = false;
 
-//------------------------------------------------------
-//Operators
+//=============================================================
+//--------------------------OPERATORS--------------------------
 // var++ var-- ++var --var
 // + - * / %
 // = += -= *= /= %=
+// && == ||
 
 lungo++;
 J /= 3;
 s1 = b*200;
 
-//------------------------------------------------------
-// Output, printing:
+
+//=============================================================
+//---------------------------OUTPUT----------------------------
 // System.out.println("string here"+variable);
 
 System.out.println("variables: " lungo + " " + (j+J) + MGMT + " " + fastisio + " " + numeroThree);
- 
 
+
+//=============================================================
+//---------------------------CLASSES---------------------------
+// <visibility> class Classname {
+//   public void Classname {} } <- Constructor method
+
+public class Testeclass {
+
+	private String nomeinstancia;
+	private final cteinstancia;
+
+	public Testeclass(String nomeinstancia, final cteinstancia) {
+		this.nomeinstancia = nomeinstancia;
+		this.cteinstancia = cteinstancia;
+	}
 }
+
+//=============================================================
+//---------------------------METHODS---------------------------
+// <visibility> <returntype> methodname(<argument1tipe> <argument1>, <argument2tipe> <argument2>) {
+// return returnvariable;
+// }
+ 
+	}
+}
+
+//=============================================================
+/** reserved keywords:
+* abstract assert boolean break byte case catch char class const continue default do double else enum extends final finally
+* float goto if implements import instanceof int interface long native new package private protected public return short static strictfp
+* super switch synchronized this throw throws transient try void volatile
+*/
